e6d92ec44d531e4bf1299170345c1c3fd543d52e
cfultz <caleb@fultz.dev>
2024-01-06 18:51:01 -0500

    Added working scripts for players and ball

---
+++ scripts/player_2_paddle.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+@export var speed = 500
+
+func _physics_process(delta):
+	var movement = Vector2.ZERO
+	if Input.is_action_pressed("up_p2"):
+		movement = Vector2.UP
+	elif Input.is_action_pressed("down_p2"):
+		movement = Vector2.DOWN
+		
+	linear_velocity = movement * speed
+
