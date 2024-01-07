1d76077784d47a78a026e636c601a34cfb6df81c
cfultz <caleb@fultz.dev>
2024-01-06 19:42:43 -0500

    Working game

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,25 @@
-extends Node
+extends Node2D
 
+var player1_points = 0
+var player2_points = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var player1_paddle = $"Player 1 Paddle"
+@onready var player2_paddle = $"Player 2 Paddle"
+@onready var ball = $Ball as Ball
 
+func _on_playe1_scored():
+	player1_points += 1
+	reset_game_state()
+	
+func _on_player2_scored():
+	player2_points += 1
+	reset_game_state()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func reset_game_state():
+	player1_paddle.global_position.y = 0
+	player2_paddle.global_position.y = 0
+	ball.global_position = Vector2.ZERO
+	ball.velocity = Vector2.ZERO
+	player1_paddle.linear_velocity = Vector2.ZERO
+	player2_paddle.linear_velocity = Vector2.ZERO
+	ball.start_ball()
--- scripts/player_2_paddle.gd
+++ scripts/player_2_paddle.gd
@@ -1,8 +1,13 @@
 extends RigidBody2D
 
 @export var speed = 500
+@export var paddle_speed = 35000
+@export var ball = Ball
 
 func _physics_process(delta):
+		human_player()
+
+func human_player():
 	var movement = Vector2.ZERO
 	if Input.is_action_pressed("up_p2"):
 		movement = Vector2.UP
@@ -10,4 +15,5 @@ func _physics_process(delta):
 		movement = Vector2.DOWN
 		
 	linear_velocity = movement * speed
+
 
