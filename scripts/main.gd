441858821c48a45a31c8170a3314d7192aa01e74
cfultz <caleb@fultz.dev>
2024-01-07 13:20:22 -0500

    Few Changes

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 var player1_points = 0
 var player2_points = 0
+var paused = false
 
 @onready var player1_paddle = $"Player 1 Paddle"
 @onready var player2_paddle = $"Player 2 Paddle"
 @onready var ball = $Ball as Ball
 @onready var ui = $Ui as UI
+@onready var pause_menu = $PauseMenu
+
 
 func _on_playe1_scored():
 	player1_points += 1
@@ -21,8 +24,24 @@ func _on_player2_scored():
 func reset_game_state():
 	player1_paddle.global_position.y = 0
 	player2_paddle.global_position.y = 0
+	player1_paddle.global_position.x = 0
+	player2_paddle.global_position.x = 0
 	ball.global_position = Vector2.ZERO
 	ball.velocity = Vector2.ZERO
 	player1_paddle.linear_velocity = Vector2.ZERO
 	player2_paddle.linear_velocity = Vector2.ZERO
 	ball.start_ball()
+	
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+	
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
--- scripts/player_2_paddle.gd
+++ scripts/player_2_paddle.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
+
 @export var speed = 500
-@export var paddle_speed = 35000
+@export var paddle_speed = 45000
 @export var ball = Ball
 
 func _physics_process(delta):
 		human_player()
+		
+
 # Setup to implement AI player
 func human_player():
 	var movement = Vector2.ZERO
@@ -13,7 +16,5 @@ func human_player():
 		movement = Vector2.UP
 	elif Input.is_action_pressed("down_p2"):
 		movement = Vector2.DOWN
-		
+
 	linear_velocity = movement * speed
-
-
